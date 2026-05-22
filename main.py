929807e059b87c82856aab2d670e1b1b13f0c261
legenddiboxe2-collab <legenddiboxe2@gmail.com>
2026-05-22 10:41:29 +0545

    Update main.py

--- main.py
+++ main.py
@@ -3684,7 +3684,7 @@ def get_math_result(input_expr):
 def send_likes(uid):
     try:
         likes_api_response = requests.get(
-             f"http://panel.thug4ff.xyz:6044/like?uid=${uid}&region=ag&key=diboxe147",
+             f"http://panel.thug4ff.xyz:6044/like?uid={uid}&region=ag&key=diboxe147",
              timeout=15
              )
       
